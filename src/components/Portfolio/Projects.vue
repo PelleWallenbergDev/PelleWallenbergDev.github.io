50488d5cd37f1edac17508b49d604be517eed590
pelwa <pelwa287@student.liu.se>
2025-02-26 11:39:35 +0100

    started work on portfolio page

--- src/components/Portfolio/Projects.vue
+++ src/components/Portfolio/Projects.vue
@@ -4,9 +4,63 @@
 
 <template>
     <div class="main"> 
-        
+        <div class="text">
+            <p>Hi my name is Pelle Wallenberg. Im a student at <a href="https://liu.se/">Linköping University</a>. I study Innovative programming, 
+                the innovative part being that we often work with different tools to create project. 
+                This makes us build up our portfolio as well as our programming expertise.
+            </p>
+            <break/>
+            <p>
+                I have created different project such as games, websites using frameworks, my own programming language using a tree based node system and more!
+                Ill also present different courses I have taken to give extra information about what I know and have done.
+            </p>
+        </div>
+        <div class="imgconatiner">p</div>
     </div>
 </template>
 
 <style>
+ .main{
+    color: black;
+    margin: 15rem;
+    margin-top: 5rem;
+    background-color: var(--blue);
+    margin-bottom: 500px;
+    display: flex;
+    padding: 3rem;
+    line-height: 1.1;
+
+    border-radius: 12px;
+    box-shadow: var(--blue) 0px 6px 24px 0px, var(--black) 0px 0px 0px 1px;
+ }
+
+ .main p{
+    font-family: "Teko", serif;
+    font-optical-sizing: auto;
+    font-weight: 400;
+    font-style: normal;
+    color: black;
+    font-size: 1.5rem;
+    margin-bottom: 0.6rem;
+ }
+
+ .main a{
+    all:unset
+ }
+
+ .imgcontainer
+ {
+    width: 50%;
+    height: 40rem;
+ }
+
+
+ /*p,h1,h2{
+    font-family: "Teko", serif;
+    font-optical-sizing: auto;
+    font-weight: 400;
+    font-style: normal;
+    color: #ebdbb2;
+  }*/
+
 </style>
--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -1,26 +1,23 @@
 <script setup lang="ts">
     //@ts-ignore
     import wip from "@/components/WorkInProgress.vue" 
+    //@ts-ignore
+    import projects from '@/components/Portfolio/Projects.vue'
 </script>
 
 <template>
     <header>
         <link href="https://fonts.googleapis.com/css2?family=Teko&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Hahmlet:wght@100..900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=STIX+Two+Text:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
     </header>
     
     <main>
-        <wip/>
+        <projects/>
 
     </main>
 </template>
 
 <style>
 
-p,h1,h2{
-    font-family: "Teko", serif;
-    font-optical-sizing: auto;
-    font-weight: 400;
-    font-style: normal;
-    color: #ebdbb2;
-  }
 </style>
